3fe87dbe472d8c809744a695b6620fbd094c7240
Christoph Haas <email@christoph-haas.de>
2012-11-28 22:32:13 +0100

    Simplification and improvement

--- setup.py
+++ setup.py
@@ -1,25 +1,36 @@
-#!/usr/bin/env python
+try:
+    from setuptools import setup, find_packages
 
-from distutils.core import setup
+import sys, os
 
-setup(name='paginate',
-      version='0.4.0',
-      description='Divides large result sets into pages',
-      author='Christoph Haas',
-      author_email='email@christoph-haas.de',
-      url='http://workaround.org/paginate/',
-      packages=['paginate'],
-      long_description="""
-This module helps divide up large result sets into pages or chunks.
-The user gets displayed one page at a time and can navigate to other pages.
-It is especially useful when developing web interfaces and showing the
-users only a selection of information at a time.
-""",
-    classifiers=["Development Status :: 4 - Beta",
-                 "Intended Audience :: Developers",
-                 "License :: OSI Approved :: BSD License",
-                 "Programming Language :: Python",
-                 "Topic :: Software Development :: Libraries :: Python Modules",
-               ],
-     )
+setup(
+    name='paginate',
+    version='0.4.0',
+    description="Divides large result sets into pages for easier browsing",
+    long_description=long_description,
+    long_description="""
+        This module helps divide up large result sets into pages or chunks.
+        The user gets displayed one page at a time and can navigate to other pages.
+        It is especially useful when developing web interfaces and showing the
+        users only a selection of information at a time.
+        """,
+
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+    keywords='pagination paginate pages',
+    author='Christoph Haas',
+    author_email='email@christoph-haas.de',
+    url='https://github.com/Signum/paginate',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points=""" """,
+)
 
