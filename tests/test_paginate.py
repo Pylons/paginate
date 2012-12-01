c168ed3d5393f29c9d030374433bd61af78f2607
Christoph Haas <email@christoph-haas.de>
2012-12-02 00:36:00 +0100

    Tests partially fixed

--- tests/test_paginate.py
+++ tests/test_paginate.py
@@ -4,9 +4,30 @@ import unittest
 
 from nose.plugins.skip import SkipTest
 from nose.tools import eq_, raises
-from webob.multidict import MultiDict
 
 import paginate
+
+# TODO: test Page(dict(one=1)) -> TypeError: Your collection of type <class 'dict'> cannot be handled by paginate.
+
+# pager()        link_attr={'class':'pager_link'}, curpage_attr={'class':'pager_curpage'},
+#   dotdot_attr={'class':'pager_dotdot'},
+
+
+def test_make_html_tag1():
+    """Test the HTML tag generator with a SPAN tag."""
+    tag = paginate.make_html_tag('span',style='green')
+    assert tag == '<span style="green">'
+
+def test_make_html_tag2():
+    """Test the HTML tag generator with an A tag."""
+    tag = paginate.make_html_tag('a',href='http://example.org/')
+    assert tag == '<a href="http://example.org/">'
+
+def test_make_html_tag3():
+    """Test the HTML tag generator with a CSS-classed DIV tag."""
+    tag = paginate.make_html_tag('div', _class='red', id='maindiv')
+    assert tag == '<div class="red" id="maindiv">'
+
 
 
 def test_empty_list():
@@ -23,8 +44,8 @@ def test_empty_list():
     assert page.items_per_page == 20
     assert page.item_count == 0
     assert page.page_count == 0
-    assert page.pager() == ''
-    assert page.pager(show_if_single_page=True) == ''
+    assert page.pager(url="http://example.org/page=$page") == ''
+    assert page.pager(url="http://example.org/page=$page", show_if_single_page=True) == ''
 
 def test_one_page():
     """Test that we fit 10 items on a single 10-item page."""
@@ -40,8 +61,9 @@ def test_one_page():
     assert page.items_per_page == 10
     assert page.item_count == 10
     assert page.page_count == 1
-    assert page.pager() == ''
-    assert page.pager(show_if_single_page=True) == '<span class="pager_curpage">1</span>'
+    assert page.pager(url="http://example.org/page=$page") == ''
+    assert page.pager(url="http://example.org/page=$page",
+                      show_if_single_page=True) == '<span class="pager_curpage">1</span>'
 
 def url_generator(**kw):
     #return update_params("/content", **kw)
@@ -81,36 +103,37 @@ def test_make_page_url():
     params = {"foo": "bar", "page": "1"}
     purl = paginate.make_page_url("/articles", params, 2)
     eq_(purl, "/articles?foo=bar&page=2")
-    params = MultiDict({"foo": "bar", "page": "1"})
-    params.add("foo", "bar2")
-    purl = paginate.make_page_url("/articles", params, 2)
-    eq_(purl, "/articles?foo=bar&foo=bar2&page=2")
+    #params = MultiDict({"foo": "bar", "page": "1"})
+    #params.add("foo", "bar2")
+    #purl = paginate.make_page_url("/articles", params, 2)
+    #eq_(purl, "/articles?foo=bar&foo=bar2&page=2")
 
-def test_pageurl():
-    purl = paginate.PageURL("/articles", {})
-    eq_(purl(2), "/articles?page=2")
-    purl = paginate.PageURL("/articles", {"foo": "bar"})
-    eq_(purl(2), "/articles?foo=bar&page=2")
-    params = {"foo": "bar", "page": "1"}
-    purl = paginate.PageURL("/articles", params)
-    eq_(purl(2), "/articles?foo=bar&page=2")
-
-class DummyRequest(object):
-    """A fake ``webob.Request`` for test_pageurl_webob``."""
-    def __init__(self, application_url, path, GET):
-        self.application_url = application_url
-        self.path = path
-        self.GET = GET
-
-def test_pageurl_webob():
-    path = "/articles"
-    application_url = "http://localhost:5000" + path
-    params = MultiDict({"blah": "boo"})
-    request = DummyRequest(application_url, path, params)
-    purl = paginate.PageURL_WebOb(request)
-    eq_(purl(2), "/articles?blah=boo&page=2")
-    purl = paginate.PageURL_WebOb(request, qualified=True)
-    eq_(purl(2), "http://localhost:5000/articles?blah=boo&page=2")
+#def test_pageurl():
+#    purl = paginate.PageURL("/articles", {})
+#    eq_(purl(2), "/articles?page=2")
+#    purl = paginate.PageURL("/articles", {"foo": "bar"})
+#    eq_(purl(2), "/articles?foo=bar&page=2")
+#    params = {"foo": "bar", "page": "1"}
+#    purl = paginate.PageURL("/articles", params)
+#    eq_(purl(2), "/articles?foo=bar&page=2")
+#
+#class DummyRequest(object):
+#    """A fake ``webob.Request`` for test_pageurl_webob``."""
+#    def __init__(self, application_url, path, GET):
+#        self.application_url = application_url
+#        self.path = path
+#        self.GET = GET
+#
+#def test_pageurl_webob():
+#    pass
+    #path = "/articles"
+    #application_url = "http://localhost:5000" + path
+    #params = MultiDict({"blah": "boo"})
+    #request = DummyRequest(application_url, path, params)
+    #purl = paginate.PageURL_WebOb(request)
+    #eq_(purl(2), "/articles?blah=boo&page=2")
+    #purl = paginate.PageURL_WebOb(request, qualified=True)
+    #eq_(purl(2), "http://localhost:5000/articles?blah=boo&page=2")
 
 
 class UnsliceableSequence(object):
@@ -147,38 +170,38 @@ class TestCollectionTypes(unittest.TestCase):
         paginate.Page(UnsliceableSequence2(self.rng))
 
 
-class TestSQLAlchemyCollectionTypes(unittest.TestCase):
-    def setUp(self):
-        try:
-            import sqlalchemy as sa
-            import sqlalchemy.orm as orm
-        except ImportError:
-            raise SkipTest()
-        self.engine = engine = sa.create_engine("sqlite://") # Memory database
-        self.sessionmaker = orm.sessionmaker(bind=engine)
-        self.metadata = metadata = sa.MetaData(bind=engine)
-        self.notes = notes = sa.Table("Notes", metadata,
-            sa.Column("id", sa.Integer, primary_key=True))
-        class Note(object):
-            pass
-        self.Note = Note
-        notes.create()
-        orm.mapper(Note, notes)
-        insert = notes.insert()
-        records = [{"id": x} for x in range(1, 101)]
-        engine.execute(insert, records)
-            
-    def tearDown(self):
-        import sqlalchemy as sa
-        import sqlalchemy.orm as orm
-        orm.clear_mappers()
-        self.notes.drop()
-
-    def test_sqlalchemy_orm(self):
-        session = self.sessionmaker()
-        q = session.query(self.Note).order_by(self.Note.id)
-        page = paginate.Page(q)
-        records = list(page)
-        eq_(records[0].id, 1)
-        eq_(records[-1].id, 20)
+#class TestSQLAlchemyCollectionTypes(unittest.TestCase):
+#    def setUp(self):
+#        try:
+#            import sqlalchemy as sa
+#            import sqlalchemy.orm as orm
+#        except ImportError:
+#            raise SkipTest()
+#        self.engine = engine = sa.create_engine("sqlite://") # Memory database
+#        self.sessionmaker = orm.sessionmaker(bind=engine)
+#        self.metadata = metadata = sa.MetaData(bind=engine)
+#        self.notes = notes = sa.Table("Notes", metadata,
+#            sa.Column("id", sa.Integer, primary_key=True))
+#        class Note(object):
+#            pass
+#        self.Note = Note
+#        notes.create()
+#        orm.mapper(Note, notes)
+#        insert = notes.insert()
+#        records = [{"id": x} for x in range(1, 101)]
+#        engine.execute(insert, records)
+#            
+#    def tearDown(self):
+#        import sqlalchemy as sa
+#        import sqlalchemy.orm as orm
+#        orm.clear_mappers()
+#        self.notes.drop()
+#
+#    def test_sqlalchemy_orm(self):
+#        session = self.sessionmaker()
+#        q = session.query(self.Note).order_by(self.Note.id)
+#        page = paginate.Page(q)
+#        records = list(page)
+#        eq_(records[0].id, 1)
+#        eq_(records[-1].id, 20)
 
