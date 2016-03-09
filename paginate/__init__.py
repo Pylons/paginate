9704e2d8f42a9b3ece55c4d16d51bcb5e8bb2abb
Marcin Lulek <info@webreactor.eu>
2016-03-09 19:32:52 +0100

    paginate: update __version__

--- paginate/__init__.py
+++ paginate/__init__.py
@@ -107,10 +107,10 @@ page's items by their index number please note that you have to subtract 1.
 """
 
 __author__ = "Christoph Haas"
-__copyright__ = "Copyright 2007-2015 Christoph Haas and contributors"
+__copyright__ = "Copyright 2007-2016 Christoph Haas and contributors"
 __credits__ = ["Mike Orr"]
 __license__ = "MIT"
-__version__ = "0.5.0"
+__version__ = "0.5.3"
 __maintainer__ = "Marcin Lulek, Luke Crooks"
 __email__ = "info@webreactor.eu, luke@pumalo.org"
 __status__ = "Beta"
