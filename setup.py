16c5613f6e2d1553b45bc73c178185af6878e516
Marcin Lulek <info@webreactor.eu>
2016-11-22 13:53:54 +0100

    fixes metadata on pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
+import os
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGELOG.txt')) as f:
+    CHANGES = f.read()
 
 setup(
     name='paginate',
-    version='0.5.5',
+    version='0.5.6',
     description="Divides large result sets into pages for easier browsing",
-    long_description="""
-        This module helps divide up large result sets into pages or chunks.
-        The user gets displayed one page at a time and can navigate to other pages.
-        It is especially useful when developing web interfaces and showing the
-        users only a selection of information at a time.
-        """,
-
+    long_description=README + '\n\n' + CHANGES,
     # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         'Programming Language :: Python',
@@ -18,7 +20,11 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        ],
+        'Framework :: Pyramid',
+        'Framework :: Flask',
+        'Framework :: Pylons',
+        'Framework :: Django',
+    ],
     keywords='pagination paginate pages',
     author='Christoph Haas',
     author_email='email@christoph-haas.de',
